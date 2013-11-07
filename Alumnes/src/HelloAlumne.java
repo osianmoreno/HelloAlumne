814d36c0c347d853407c942197256700473ee91a
osianmoreno <morenosoftware@gmail.com>
2013-11-07 21:34:56 +0100

    Prueba !

--- Alumnes/src/HelloAlumne.java
+++ Alumnes/src/HelloAlumne.java
@@ -2,7 +2,7 @@ public class HelloAlumne{
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hola soc Osian Moreno Alcaide");
+		System.out.println("Hola soc Osian  Moreno Alcaide");
 		
 	}
 	
